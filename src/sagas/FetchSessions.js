f92d63578591a83031e95ad800376cc164d00114
Ingar Abrahamsen <ingar@abrahams1.com>
2017-04-06 22:28:44 +0200

    feat: setup sagas

---
+++ src/sagas/FetchSessions.js
@@ -0,0 +1,19 @@
+// @flow
+
+import { call, put, takeLatest, select } from 'redux-saga/effects';
+import { sessionBySlug } from './SleepingPillApi';
+import { sessionsFetchError, sessionsFetchSuccess, SESSIONS } from '../actions/session';
+
+function* fetchSessionsRequest() {
+  try {
+    const selectedConf = yield select((state) => (state.conferences.selected));
+    const sessions = yield call(() => sessionBySlug(selectedConf));
+    yield put(sessionsFetchSuccess(sessions))
+  } catch (e) {
+    yield put(sessionsFetchError(e.message))
+  }
+}
+
+export default function* fetchSessions() {
+  yield takeLatest(SESSIONS.FETCH_REQUEST, fetchSessionsRequest);
+}
