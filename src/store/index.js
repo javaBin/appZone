a7f5b024915fb1bc9c07d954c78f713dbe4edfca
Eivind Hyldmo <eivind.hyldmo@avanade.com>
2017-04-25 19:11:02 +0200

    Added devtools

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,14 @@
 import { applyMiddleware, createStore } from 'redux';
 import createSagaMiddleware from 'redux-saga';
+import { composeWithDevTools } from 'remote-redux-devtools';
 import sagas from '../sagas';
 import reducer from '../reducers';
 
 const sagaMiddleware = createSagaMiddleware();
 
-const store = createStore(reducer, applyMiddleware(sagaMiddleware));
+const store = createStore(reducer, composeWithDevTools(
+    applyMiddleware(sagaMiddleware)
+))
 
 sagaMiddleware.run(sagas);
 
