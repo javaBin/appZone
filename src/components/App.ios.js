24ddab57cfc6f5c7ed785c54f7ece1b587a862ff
Eivind Hyldmo <eivind.hyldmo@avanade.com>
2017-05-09 20:38:29 +0200

    Fixed linter errors and added react/native rules

--- src/components/App.ios.js
+++ src/components/App.ios.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import MapScreen from './maps/MapScreen';
 import VideoScreen from './video/VideoScreen';
