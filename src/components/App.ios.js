f19fe0597b6747c444075971a2c06b784df608c6
Khiem-Kim Ho Xuan <khiem_89@hotmail.com>
2017-05-23 21:02:09 +0200

    added a working tab for ios

--- src/components/App.ios.js
+++ src/components/App.ios.js
@@ -1,3 +1,4 @@
+<<<<<<< 206eb96f24538de88755b6fca0c4e9a4b7dc9eec
 <<<<<<< 55255650cd5d9c2dd5625f97fd421b780896abc4
 <<<<<<< ff05c1e5cc0b1b65a441d7f53bf4491edc643e7f
 import React from 'react';
@@ -13,6 +14,9 @@ const App = () => (
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 =======
+=======
+<<<<<<< HEAD
+>>>>>>> added a working tab for ios
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 >>>>>>> fixed lint errors
@@ -47,6 +51,27 @@ const styles = StyleSheet.create({
     color: '#333333',
     marginBottom: 5
   }
+=======
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, DrawerLayoutAndroid } from 'react-native';
+import { Button, Card, Toolbar, Drawer } from 'react-native-material-design';
+import TabBarView from './tab-bar-navigation/TabBarView'
+
+class App extends Component {
+    render() {
+        return (
+            <TabBarView style={styles.container} />
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+>>>>>>> added a working tab for ios
 });
 
 export default App
---
+++ src/components/tab-bar-navigation/TabBarView.ios.js
@@ -0,0 +1,122 @@
+'use strict';
+
+import React from 'react';
+import {
+  TabBarIOS,
+  Navigator,
+  View,
+  Text,
+  StyleSheet
+
+} from 'react-native';
+
+// TODO add switchTab
+import { connect } from 'react-redux';
+
+class TabBarView extends React.Component {
+  static title = '<TabBarIOS>';
+  static description = 'Tab-based navigation.';
+  static displayName = 'TabBarExample';
+
+  state = {
+    selectedTab: 'redTab',
+    notifCount: 0,
+    presses: 0,
+  };
+
+    props: {
+    tab: Tab;
+    day: Day;
+    onTabSelect: (tab: Tab) => void;
+    navigator: Navigator;
+  };
+
+  onTabSelect(tab: Tab) {
+    if (this.props.tab !== tab) {
+      this.props.onTabSelect(tab);
+    }
+  }
+
+
+  _renderContent = (color: string, pageText: string, num?: number) => {
+    return (
+      <View style={[styles.tabContent, {backgroundColor: color}]}>
+        <Text style={styles.tabText}>{pageText}</Text>
+        <Text style={styles.tabText}>{num} re-renders of the {pageText}</Text>
+      </View>
+    );
+  };
+
+  render() {
+    return (
+      <TabBarIOS
+        unselectedTintColor="yellow"
+        tintColor="white"
+        unselectedItemTintColor="red"
+        barTintColor="darkslateblue">
+        <TabBarIOS.Item
+          title="Blue Tab"
+          selected={this.state.selectedTab === 'blueTab'}
+          onPress={() => {
+            this.setState({
+              selectedTab: 'blueTab',
+            });
+          }}>
+          {this._renderContent('#414A8C', 'Blue Tab')}
+        </TabBarIOS.Item>
+        <TabBarIOS.Item
+          systemIcon="history"
+          badge={this.state.notifCount > 0 ? this.state.notifCount : undefined}
+          badgeColor="black"
+          selected={this.state.selectedTab === 'redTab'}
+          onPress={() => {
+            this.setState({
+              selectedTab: 'redTab',
+              notifCount: this.state.notifCount + 1,
+            });
+          }}>
+          {this._renderContent('#783E33', 'Red Tab', this.state.notifCount)}
+        </TabBarIOS.Item>
+        <TabBarIOS.Item
+          renderAsOriginal
+          title="More"
+          selected={this.state.selectedTab === 'greenTab'}
+          onPress={() => {
+            this.setState({
+              selectedTab: 'greenTab',
+              presses: this.state.presses + 1
+            });
+          }}>
+          {this._renderContent('#21551C', 'Green Tab', this.state.presses)}
+        </TabBarIOS.Item>
+      </TabBarIOS>
+    );
+  }
+}
+
+var styles = StyleSheet.create({
+  tabContent: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  tabText: {
+    color: 'white',
+    margin: 50,
+  },
+});
+
+function select(store) {
+  return {
+    tab: store.navigation.tab,
+    day: store.navigation.day
+  };
+}
+
+function actions(dispatch) {
+  return {
+    onTabSelect: (tab) => dispatch(switchTab(tab)),
+  };
+}
+
+module.exports = TabBarView;
+
