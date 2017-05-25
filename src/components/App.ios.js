af6c3a26e1d755f8168e4d7ed7de403278c7d9bd
Khiem-Kim Ho Xuan <khiem_89@hotmail.com>
2017-05-25 10:58:21 +0200

    fixed lint errors

--- src/components/App.ios.js
+++ src/components/App.ios.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
 
 import SettingsScreen from './settings/SettingsScreen';
 
