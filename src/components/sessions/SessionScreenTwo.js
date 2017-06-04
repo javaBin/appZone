846a755d0d605495da4715002aa0fe029af8d5c3
Stine Grumheden <stinemarie86@gmail.com>
2017-06-04 16:20:33 +0200

    removed unused code

--- src/components/sessions/SessionScreenTwo.js
+++ src/components/sessions/SessionScreenTwo.js
@@ -3,19 +3,11 @@ import { Text, View } from 'react-native';
 import { connect } from 'react-redux';
 
 export default class SessionScreenTwo extends React.Component {
-  // static navigationOptions = ({ navigation }) => ({
-  //   sessions: navigation.state.params,
-  // })
   render() {
-    // const { params } = this.props.navigation.state;
     return (
       <View>
-        <Text>Her er den andre taben</Text>
+        <Text>Sessions screen two</Text>
       </View>
     )
   }
 }
-
-// export default connect(
-//   (state) => ({sessions: state.sessions})
-// )(SessionScreenTwo)
