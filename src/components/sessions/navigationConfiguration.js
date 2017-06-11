e43572bf112932a28b8eed175137c5a616ee9418
Stine Grumheden <stinemarie86@gmail.com>
2017-06-11 16:52:21 +0200

    added session list and detail

--- src/components/sessions/navigationConfiguration.js
+++ src/components/sessions/navigationConfiguration.js
@@ -1,15 +1,15 @@
 import { StackNavigator } from 'react-navigation'
 // Screens
-import SessionScreen from './SessionScreen';
-import SessionScreenTwo from './SessionScreenTwo';
+import SessionList from './SessionList';
+import SessionDetail from './SessionDetail';
 
 const routeConfiguration = {
-  SessionScreen: { screen: SessionScreen },
-  SessionScreenTwo: { screen: SessionScreenTwo },
-}
+  SessionList: { screen: SessionList },
+  SessionDetail: { screen: SessionDetail },
+};
 // going to disable the header for now
 const stackNavigatorConfiguration = {
   headerMode: 'none',
-  initialRoute: 'SessionScreen'
+  initialRoute: 'SessionList'
 }
 export const SessionNavigatorTab = StackNavigator(routeConfiguration,stackNavigatorConfiguration)
