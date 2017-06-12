fa2b79d8964a5deb7cc976c81530b64b71fa0edd
Stine Grumheden <stinemarie86@gmail.com>
2017-06-12 22:32:21 +0200

    added styling to tab bar and session detail

--- src/components/sessions/SessionDetail.js
+++ src/components/sessions/SessionDetail.js
@@ -1,43 +1,97 @@
 import React from 'react';
-import { Text, View, Button, ScrollView } from 'react-native';
+import { Text, StyleSheet, View, Button, ScrollView } from 'react-native';
 import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
 
 import moment from 'moment';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import style from '../../common/style.js'
 
+const styles = StyleSheet.create({
+  sessionHeader: {
+    flex: 1,
+    padding: 5,
+    backgroundColor: style.colors.background,
+  },
+  heading1: {
+    color: style.colors.color1,
+    fontSize: 30,
+    fontFamily: 'jura-bold',
+  },
+  heading2: {
+    color: style.colors.color2,
+    fontSize: 25,
+    fontFamily: 'jura-bold',
+    padding: 5,
+  },
+  headingSpeakerName: {
+    color: style.colors.color2,
+    fontWeight: "500",
+    padding: 5,
+  },
+  textStyleHeder: {
+    color: style.colors.color1,
+  },
+  textStyle: {
+    color: style.colors.primary,
+    padding: 5,
+  },
+  container: {
+    backgroundColor: style.colors.backgroundSecondary,
+  },
+  keywordContainer: {
+    backgroundColor: style.colors.color3,
+    color: style.colors.backgroundSecondary,
+    fontFamily: 'jura-bold',
+    padding: 3, 
+    margin: 5, 
+  }
+});
 
 export default class SessionDetail extends React.Component {  
+  
   render() {
-
     const { params } = this.props.navigation.state;
     let fromTime = moment(new Date(params.sessionData.startTime)).format('dddd, MMMM DD HH:mm');
     let toTime = moment (new Date(params.sessionData.endTime)).format('HH:mm');
     return (
       <ScrollView>
-        <Text>Tittel: {params.sessionData.title}</Text>
-        <Text>Format: {params.sessionData.format}</Text>
-        <Text>{fromTime} - {toTime}</Text>
-        <Text>Publikum: {params.sessionData.intendedAudience}</Text>
-        <Text>Sammendrag: {params.sessionData.abstract}</Text>
-        { 
-          params.sessionData.speakers.map( (speaker, index) => {
-            return (
-              <View key={index}>
-                <Text>Speaker: { speaker.name }</Text>
-                <Text>Bio: { speaker.bio }</Text>
-              </View>);
-          })
-        }        
-        <Text>Nøkkelord: {
-          params.sessionData.keywords.toString()
-        }</Text> 
-        <Button
-          onPress={() => this.props.navigation.navigate('SessionList')}
-          title="Gå tilbake"
-        />
-    </ScrollView>
+        <View style={ styles.sessionHeader }>
+          <View style={{ flex: 1, flexDirection: 'row' }}>
+            <Icon name="arrow-left" style={{padding: 15}} size={40} color={style.colors.color4} 
+              onPress={() => this.props.navigation.navigate('SessionList')}/>   
+            <Icon name="star-o" style={{padding: 15}}size={40} color={style.colors.color4}/>   
+          </View>
+          <Text style={ styles.heading1 }>{ (params.sessionData.title).toUpperCase() }</Text>
+          <View style={{flex: 1, flexDirection: 'row'}}>
+            <View>
+              <Text style={ styles.textStyleHeder }>{ params.sessionData.room } { params.sessionData.format }</Text>
+              <Text style={{color: style.colors.color1}}>{ fromTime } - { toTime }</Text>
+            </View>
+          </View>
+        </View>
+        <View style={ styles.container }>
+          <Text style={ styles.textStyle }>{ params.sessionData.abstract }</Text>
+          <Text style={ styles.textStyle }>{ params.sessionData.intendedAudience }</Text>
+          <Text style={ styles.heading2 }>SPEAKER:</Text>
+          { 
+            params.sessionData.speakers.map( (speaker, index) => {
+              return (
+                <View key={index}>
+                  <Text style={ styles.headingSpeakerName }>{ speaker.name }</Text>
+                  <Text style={ styles.textStyle }>{ speaker.bio }</Text>
+                </View>);
+            })
+          }
+          <Text style={ styles.heading2 }>KEYWORDS:</Text>        
+          <View>
+          {
+            params.sessionData.keywords.map( (keyword, index) => {
+              return <Text style={ styles.keywordContainer } key={index}>{keyword}</Text>
+            })
+          }
+          </View>
+        </View>
+      </ScrollView>
     )
   }
 }
-
-// dispatch params.sessiondata.sessionId
--- src/components/tab-bar-navigation/navigationConfiguration.js
+++ src/components/tab-bar-navigation/navigationConfiguration.js
@@ -4,6 +4,8 @@ import { HomeNavigatorTab } from '../home/navigationConfiguration'
 import { SessionNavigatorTab } from '../sessions/navigationConfiguration';
 import { SettingsNavigatorTab } from '../settings/navigationConfiguration';
 
+import style from '../../common/style.js'
+
 const routeConfiguration  = ({
     HomeNavigatorTab: { screen: HomeNavigatorTab },
     SessionNavigatorTab: { screen: SessionNavigatorTab }, 
@@ -11,14 +13,26 @@ const routeConfiguration  = ({
 });
 
 const tabBarConfiguration = {
+    tabBarPosition: 'bottom',
     tabBarOptions: {
-    // tint color is passed to text and icons (if enabled) on the tab bar
-    activeTintColor: 'white',
-    inactiveTintColor: 'blue',
-// background color is for the tab component
-    activeBackgroundColor: 'blue',
-    inactiveBackgroundColor: 'white',
-    }
+      showIcon: false,
+      showLabel: true,
+  // tint color is passed to text and icons (if enabled) on the tab bar
+      activeTintColor: style.colors.primary,
+      inactiveTintColor: style.colors.secondary,
+  // background color is for the tab component
+      activeBackgroundColor: style.colors.background,
+      inactiveBackgroundColor: style.colors.background,
+      activeBorderBottomColor: style.colors.color1,
+      style: {
+        backgroundColor: style.colors.background,
+        borderTopWidth: 2,
+        borderTopColor: style.colors.color1,        
+      },
+      indicatorStyle: {
+        backgroundColor: style.colors.color4,
+      }
+   }
 }
 
 export const tabBarReducer = (state, action) => {
