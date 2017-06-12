2e1472794232f00fea60f161372c66c625ef1087
Stine Grumheden <stinemarie86@gmail.com>
2017-06-12 22:32:21 +0200

    added styling to tab bar and session detail

---
+++ src/common/style.js
@@ -0,0 +1,12 @@
+module.exports = {
+  colors: {
+    primary: '#ffffff',
+    secondary: '#909090',
+    background: '#000000',
+    backgroundSecondary: '#1c2430',
+    color1: '#3abae9', // blue
+    color2: '#b63d9d', // purple
+    color3: '#30a74d', // green
+    color4: '#f28e31', // orange
+  }
+};
