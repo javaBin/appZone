2c4efdc57ac00badd79e3024ff6051d78c5165a1
Ingar Abrahamsen <ingar@abrahams1.com>
2017-07-15 23:57:16 +0200

    refactor: simplify the startup files for ios and android

--- index.ios.js
+++ index.ios.js
@@ -1,11 +1,4 @@
-import React, {Component} from 'react';
-import { AppRegistry } from 'react-native';
-import Main from './src/Main';
+import { AppRegistry } from 'react-native'
+import Main from './src/Main'
 
-export default class appZone extends Component {
-    render() {
-        return <Main />
-    }
-}
-
-AppRegistry.registerComponent('appZone', () => appZone);
+AppRegistry.registerComponent('appZone', () => Main)
