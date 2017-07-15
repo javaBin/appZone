287159af9cd0419f4be29a182ceb088fe18d7b12
Ingar Abrahamsen <ingar@abrahams1.com>
2017-07-15 23:57:16 +0200

    refactor: simplify the startup files for ios and android

--- index.android.js
+++ index.android.js
@@ -1,37 +1,4 @@
-/*import React, { Component } from 'react';
-import {
-  AppRegistry
-} from 'react-native';
+import { AppRegistry } from 'react-native'
+import Main from './src/Main'
 
-// Redux
-import { Provider } from 'react-redux'
-import store from './src/store'
-
-// Navigation
-import TabBarNavigation from './src/components/tab-bar-navigation/TabBarNavigation';
-
-export default class appZone extends Component {
-  render(){
-    return(
-      <Provider store={store}>
-        <TabBarNavigation />
-      </Provider>
-    )
-  }
-}
-AppRegistry.registerComponent('appZone', () => appZone);
-
-
-//https://medium.com/@parkerdan/react-navigation-with-complete-redux-state-management-tab-bar-and-multiple-navigators-ed30a69d9a4d*/
-
-import React, {Component} from 'react';
-import { AppRegistry } from 'react-native';
-import Main from './src/Main';
-
-export default class appZone extends Component {
-    render() {
-        return <Main />
-    }
-}
-
-AppRegistry.registerComponent('appZone', () => appZone);
+AppRegistry.registerComponent('appZone', () => Main)
--- index.ios.js
+++ index.ios.js
@@ -1,3 +1,4 @@
+<<<<<<< 4ac625ef1c7a30dbf3fda8d6432e2aee4b8b621f
 import React, { Component } from 'react';
 import { AppRegistry, View, StatusBar } from 'react-native';
 import Main from './src/Main';
@@ -35,3 +36,9 @@ export default class appZone extends Component {
 }
 
 AppRegistry.registerComponent('appZone', () => appZone);
+=======
+import { AppRegistry } from 'react-native'
+import Main from './src/Main'
+
+AppRegistry.registerComponent('appZone', () => Main)
+>>>>>>> refactor: simplify the startup files for ios and android
