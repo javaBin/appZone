822fcc2e6080889772fed0c56bfb3a3e0213f9d4
Ingar Abrahamsen <ingar@abrahams1.com>
2017-07-19 22:36:36 +0200

    refactor: remove unused reducer code

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -6,11 +6,6 @@ import settings from './settings'
 import sessions from './sessions'
 import conferences from './conferences'
 import filter from './filter'
-
-//Navigation
-import { SessionNavigatorTab } from '../components/sessions/navigationConfiguration'
-import { HomeNavigatorTab } from '../components/home/navigationConfiguration'
-import { SettingsNavigatorTab } from '../components/settings/navigationConfiguration'
 import { tabBarReducer } from '../components/tab-bar-navigation/navigationConfiguration'
 
 import type { SessionState } from './sessions'
@@ -41,15 +36,6 @@ const reducers = combineReducers({
   settings,
   filter,
   tabBar: tabBarReducer,
-
-  tabSession: (state, action) =>
-  SessionNavigatorTab.router.getStateForAction(action,state),
-
-  tabSettings : (state, action) =>
-  SettingsNavigatorTab.router.getStateForAction(action,state),
-
-  tabHome: (state, action) =>
-  HomeNavigatorTab.router.getStateForAction(action,state)
 })
 
 export default reducers
