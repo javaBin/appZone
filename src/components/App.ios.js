c449cbf1b0fba87b5586f7d0e5fac420581fb78b
Khiem-Kim Ho Xuan <khiem_89@hotmail.com>
2017-07-19 21:06:03 +0200

    prepared ios firebase

--- src/components/App.ios.js
+++ src/components/App.ios.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react'
 import { StyleSheet } from 'react-native'
 import TabBarNavigation from './tab-bar-navigation/TabBarNavigation'
+import firebase from 'react-native-firebase'
 
 class App extends Component {
     render() {
+        firebase.analytics().setAnalyticsCollectionEnabled(true);
+        firebase.analytics().setMinimumSessionDuration(15000);
         return (
             <TabBarNavigation style={styles.container} />
         )
