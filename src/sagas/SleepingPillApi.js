c388131dbbd347c4d189a0b19eff2bd1f3b46293
Ingar Abrahamsen <ingar@abrahams1.com>
2017-07-25 00:12:15 +0200

    refactor: gather all config in one file
    We will need to change some of the settings when developing (for
    instance use urls to a test environment etc). To make it easier to
    manage I moved them to a config file where we can easier control the
    values.

--- src/sagas/SleepingPillApi.js
+++ src/sagas/SleepingPillApi.js
@@ -1,11 +1,13 @@
 // @flow
 
+import config from '../config'
+
 export const allSession: () => any =
   () =>
-    fetch('https://sleepingpill.javazone.no/public/allSessions')
+    fetch(`${config.urls.sleepingPill}/public/allSessions`)
       .then((res) => res.json())
 
 export const sessionBySlug: (string) => any =
-  (slug) => fetch(`https://sleepingpill.javazone.no/public/allSessions/${slug}`)
+  (slug) => fetch(`${config.urls.sleepingPill}/public/allSessions/${slug}`)
     .then((res) => res.json())
 
