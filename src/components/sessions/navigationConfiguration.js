8a3165450d8b14a8b8aed0a63967a6da6327addd
lisa <halvorsen.lisa@gmail.com>
2017-08-15 16:54:43 +0200

    fix from rebase

--- src/components/sessions/navigationConfiguration.js
+++ src/components/sessions/navigationConfiguration.js
@@ -1,17 +1,6 @@
 import { StackNavigator } from 'react-navigation'
 // Screens
-<<<<<<< 4ea13d9cfd4d95dda0828d359572dee6e0973d15
-<<<<<<< 16be030b656664568267d3793bb24420e7de3836
-import SessionList from './list/SessionList'
-=======
-<<<<<<< 1a0ca5fa4b30dff513942bed171474b54c91ae2c
-import SessionList from './SessionList'
->>>>>>> Session feedback. WIP
-import SessionDetail from './SessionDetail'
-=======
-=======
->>>>>>> Selectable feedback. WIP
-import SessionList from './SessionList';
+import SessionList from './list/SessionList';
 import SessionDetail from './SessionDetail';
 import Feedback from '../feedback/Feedback';
 
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,39 +1,19 @@
 // @flow
-<<<<<<< 4ea13d9cfd4d95dda0828d359572dee6e0973d15
 
-<<<<<<< 16be030b656664568267d3793bb24420e7de3836
 import { combineReducers } from 'redux'
-=======
-=======
-import {combineReducers} from 'redux'
->>>>>>> Selectable feedback. WIP
-import { CONFERENCES } from '../actions/conference'
-import { SESSIONS } from '../actions/session'
-import { DAY } from '../actions/filter'
-import settings from './settings'
-import type { Session, Conference } from '../types/SleepingPill'
-import type {   PayloadAction } from '../types/Actions'
-
-<<<<<<< e435eb0e7ec2cd1ed21bfe8af798f79c16205ef0
-<<<<<<< 0fe177708fc9ced6b78ca557214e91193916c2e4
->>>>>>> Session feedback. WIP
-=======
-import { FEEDBACK } from '../actions/feedback';
->>>>>>> feedback. WIP
-=======
-import feedback from './feedback';
->>>>>>> refac
 
 import settings from './settings'
 import sessions from './sessions'
 import conferences from './conferences'
 import filter from './filter'
+import feedback from './feedback'
 import { tabBarReducer } from '../components/tab-bar-navigation/navigationConfiguration'
 
 import type { SessionState } from './sessions'
 import type { ConferencesState } from './conferences'
 import type { FilterState } from './filter'
 import type { SettingsState } from './settings'
+import type { FeedbackState } from './feedback'
 
 /**
  * Each sub reducer will have it's own sub name space. For instance the conferences
@@ -46,21 +26,19 @@ import type { SettingsState } from './settings'
 
 export type StoreState = {
   conferences: ConferencesState,
+  feedback: FeedbackState,
   session: SessionState,
   filter: FilterState,
   settings: SettingsState,
   tabBar: any
 }
 
-
-
-
 const reducers = combineReducers({
   conferences, 
+  feedback,
   sessions,
   settings,
   filter,
-  feedback,
   tabBar: tabBarReducer,
 })
 
