0323e85c4bacc6c676c6daadcceeb19dca0a389f
Stine Grumheden <stinemarie86@gmail.com>
2017-08-19 18:17:39 +0200

    fixed linter issues

--- src/components/App.android.js
+++ src/components/App.android.js
@@ -9,7 +9,7 @@ import SplashScreen from 'react-native-splash-screen'
 class App extends Component {
 
     componentDidMount() {
-        SplashScreen.hide();
+        SplashScreen.hide()
     }
 
     render () {
