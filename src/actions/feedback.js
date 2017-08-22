1a4fc3a3ed21e135542d96646b9c28a8671bd734
lisa <halvorsen.lisa@gmail.com>
2017-08-22 21:03:23 +0200

    fix lint error

--- src/actions/feedback.js
+++ src/actions/feedback.js
@@ -1,5 +1,3 @@
-
-
 export const FEEDBACK = {
   ADD: 'FEEDBACK_ADD',
   UPDATE: 'FEEDBACK_UPDATE',
@@ -7,27 +5,24 @@ export const FEEDBACK = {
   FETCH_SUCCESS: 'FETCH_SUCCESS',
   FETCH_ERROR: 'FETCH_ERROR',
   REMOVE_ERROR: 'REMOVE_ERROR'
-};
+}
 
 export const updateFeedback = 
-  (feedback) => ({type: FEEDBACK.UPDATE, payload : feedback})
+  (feedback) => ({ type: FEEDBACK.UPDATE, payload : feedback })
 
 export const addFeedback =
   (feedback) => {
-    return ({type: FEEDBACK.ADD, payload: feedback});
-  };
+    return ({ type: FEEDBACK.ADD, payload: feedback })}
 
 export const submitFeedback =
   (feedback) => {
-      return ({ type: FEEDBACK.SUBMIT, payload : feedback});
-  };
+      return ({ type: FEEDBACK.SUBMIT, payload : feedback })}
 export const feedbackFetchSuccess =
   (json) => {
-    return {type: FEEDBACK.FETCH_SUCCESS, response: json};
-  }
+    return { type: FEEDBACK.FETCH_SUCCESS, response: json }}
 
 export const feedbackFetchError =
-  (errorMsg) => ({type: FEEDBACK.FETCH_ERROR, error: errorMsg});
+  (errorMsg) => ({ type: FEEDBACK.FETCH_ERROR, error: errorMsg })
 
-export const removeError = (error) => ({type: FEEDBACK.REMOVE_ERROR, error});
+export const removeError = (error) => ({ type: FEEDBACK.REMOVE_ERROR, error })
   
