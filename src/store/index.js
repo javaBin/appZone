dc350a5eff4bd7d4a4330b968df3fc3324837924
lisa <halvorsen.lisa@gmail.com>
2017-08-22 18:30:05 +0200

    remove from merge

--- src/store/index.js
+++ src/store/index.js
@@ -4,13 +4,12 @@ import { composeWithDevTools } from 'remote-redux-devtools'
 import sagas from '../sagas'
 import reducer from '../reducers'
 
-
     const sagaMiddleware = createSagaMiddleware()
     const enhancer = composeWithDevTools({ name: 'appZone', realtime: true, port: 8083, hostname: 'localhost' })
 
     const store = createStore(reducer, enhancer(
         applyMiddleware(sagaMiddleware)
-    ));
+    ))
 
 sagaMiddleware.run(sagas)
 
