1f752feca8505cd7c41e291840d8539feacd5ef3
Ingar Abrahamsen <ingar@abrahams1.com>
2017-08-24 21:30:31 +0200

    fix: navigation from session details used goBack
    This gives the right animation when navigating from the detail view.

--- src/components/sessions/SessionDetail.js
+++ src/components/sessions/SessionDetail.js
@@ -77,7 +77,7 @@ class SessionDetail extends React.Component {
         <View style={styles.sessionHeaderWrapper}>
           <View style={{ flex: 1, flexDirection: 'row' }}>
             <Icon name="arrow-left" style={{ padding: 15 }} size={40} color={style.colors.color4}
-              onPress={() => navigation.navigate('SessionList')} />
+              onPress={() => navigation.goBack()} />
           </View>
           <Text style={styles.heading1}>{(params.sessionData.title).toUpperCase()}</Text>
           <View style={{ flex: 1, flexDirection: 'row' }}>
