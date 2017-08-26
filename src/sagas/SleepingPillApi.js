cec9d120bb7520b2cdaf8e64a9e2adc8c843ab83
lisa <halvorsen.lisa@gmail.com>
2017-08-26 13:14:58 +0200

    moved to separate file

--- src/sagas/SleepingPillApi.js
+++ src/sagas/SleepingPillApi.js
@@ -1,6 +1,5 @@
 
 // @flow
-import DeviceInfo from 'react-native-device-info'
 
 import config from '../config'
 
@@ -12,18 +11,3 @@ export const allSession: () => any =
 export const sessionBySlug: (string) => any =
   (slug) => fetch(`${config.urls.sleepingPill}/public/allSessions/${slug}`)
     .then((res) => res.json())
-
-const options = {
-	"Content-Type": "application/json",
-    headers: {
-      'Content-Type': "application/json",
-      "Voter-ID": DeviceInfo? "javazone2017" : "javazone2017"
-    }
-}
-export const feedback: (feedback) => any =
-  (feedback) => {
-    options.method = 'POST'
-    options.body = JSON.stringify(feedback.feedback)
-    return fetch(`${config.urls.devnull}/events/${feedback.eventId}/sessions/${feedback.sessionId}/feedbacks`, options)
-      .then(res => res.json())
-  }
