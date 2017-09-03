1fdf49b6b15bad9928f2767efa9de640dd854be1
Stine Grumheden <stinemarie86@gmail.com>
2017-09-03 17:30:36 +0200

    fixed linter issue

--- src/components/App.android.js
+++ src/components/App.android.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react'
 import { StyleSheet } from 'react-native'
 import TabBarNavigation from './tab-bar-navigation/TabBarNavigation'
 
-import moment from 'moment'
 import SplashScreen from 'react-native-splash-screen'
 
 class App extends Component {
