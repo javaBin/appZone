d12ff394d4c2b8b2aa65861a58ee9c5ade5bd51b
lisa <halvorsen.lisa@gmail.com>
2017-09-06 00:08:14 +0200

    add some feedback on res.status

--- src/sagas/DevNullApi.js
+++ src/sagas/DevNullApi.js
@@ -6,10 +6,19 @@ export const feedback: (feedback) => any =
       method: 'POST',
       headers: {
         'Content-Type': "application/json",
-        "Voter-ID": feedback.uuid ? feedback.uuid : "javazone2017"
+        "Voter-ID": feedback.uuid 
       },
       body: JSON.stringify(feedback.feedback)
     }
     return fetch(`${config.urls.devnull}/events/${feedback.eventId}/sessions/${feedback.sessionId}/feedbacks`, options)
-      .then(res => res.json())
-  }
+      .then(res => {
+        if (res.status === 202) {
+          return { message: 'Feedback submitted' }
+        } else if(res.status === 403) {
+          return { message : 'Please try again. Feedback opens 10 min before session ends.' }  
+        } else {
+          return res.status  
+        }
+      }
+      )
+  } 
