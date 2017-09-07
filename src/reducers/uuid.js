f208622dc4b7f9bbd118efc934912ee45b6d1538
lisa <halvorsen.lisa@gmail.com>
2017-09-07 08:37:42 +0200

    add storage of uuid in asyncStorage

--- src/reducers/uuid.js
+++ src/reducers/uuid.js
@@ -1,62 +1,24 @@
 // @flow
 
-import { AsyncStorage } from 'react-native'
 import { UUID } from '../actions/uuid'
-import type { GetUUIDAction, SetUUIDAction } from '../actions/uuid'
+import type { GetUUIDSuccessAction, SetUUIDSuccessAction } from '../actions/uuid'
 
-export type UUIDActions = GetUUIDAction | SetUUIDAction 
+export type UUIDActions = SetUUIDSuccessAction | GetUUIDSuccessAction
 export type UUIDState = {
   uuid:string
 }
 const initUUID = { uuid: '' } 
 const uuid = (state:UUIDState = initUUID, action: UUIDActions) => {
   switch(action.type) {
-    case UUID.GET : {
-      return { ...state }
+    case UUID.GET_SUCCESS : {
+      return { ...state, uuid: action.payload }  
     }
-    case UUID.SET : {
-      storeUUID(action.type, action.payload)
+    case UUID.SET_SUCCESS : {
       return { ...state, uuid: action.payload }
     }
     default:
       return { ...state } 
   }
 }
-
-const STORE_UUID_KEY = 'UUIDKEY'
-
-function storeUUID(type, uuidStr) {
-  let uuid: string = ''
-
-  AsyncStorage.getItem(STORE_UUID_KEY).then((uuidString) => {
-    uuid = JSON.parse(uuidString)
-    if (uuid !== null) {
-      switch (type) {
-        case UUID.SET:
-          uuid = uuidStr
-          break
-        default:
-          break
-      }
-
-      AsyncStorage.setItem(STORE_UUID_KEY, JSON.stringify(uuid), () => {
-      })
-    }
-    else {
-      switch (type) {
-        case UUID.SET:
-          uuid = uuidStr
-          break
-        default:
-          break
-      }
-
-      AsyncStorage.setItem(STORE_UUID_KEY, JSON.stringify(uuid), () => {
-      })
-    }
-  })
-}
-
-
 
 export default uuid
